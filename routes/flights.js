1fa6dcdd19ee2c69a5502f0d3b906bd472e4de8f
devoperations <chrishayes0130@gmail.com>
2022-04-18 20:13:36 -0700

    folder additions

---
+++ routes/flights.js
@@ -0,0 +1,19 @@
+
+   
+var express = require('express');
+var router = express.Router();
+const flightsCtrl = require('../controllers/flights');
+
+// All routes starts with: /flights (because of the mounting in server.js)
+
+// GET /flights
+router.get('/', flightsCtrl.index);
+// GET /flights/new (new functionality)
+router.get('/new', flightsCtrl.new);
+// GET /flights/:id (show functionality) 
+router.get('/:id', flightsCtrl.show);
+// POST /flights (create functionality)
+router.post('/', flightsCtrl.create);
+
+
+module.exports = router;
